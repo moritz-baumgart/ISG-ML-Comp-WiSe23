3d8bf49d7c3367ba4f7bb92f75f95525c6213c1c
Moritz Baumgart <baumgartmoritz@web.de>
2023-11-14 16:03:09 +0100

    Fixed label splitting when loading data.

--- src/data_exploration/load.py
+++ src/data_exploration/load.py
@@ -1,26 +1,25 @@
 import pandas as pd
 
 
-def get_train_data(ft_file: str, label_file: str, split_label=False):
+def get_train_data(ft_file: str, label_file: str, label_name: str, split_label=False):
     train_ft = pd.read_csv(ft_file)
     train_label = pd.read_csv(label_file)
 
     df = pd.merge(train_ft, train_label, on='Id', how='inner')
 
     if split_label:
-
         X = df.drop(columns=['Id', 'label'])
-        y = df['label']
+        y = df[label_name]
 
         return X, y
-    else:
-        return df
+    
+    return df
 
 def get_class_train_data(split_label=False):
-    return get_train_data('../data/classification/train_features.csv', '../data/classification/train_label.csv', split_label)
+    return get_train_data('../data/classification/train_features.csv', '../data/classification/train_label.csv', 'label', split_label)
 
 def get_reg_train_data(split_label=False):
-    return get_train_data('../data/regression/train_features.csv', '../data/regression/train_label.csv', split_label)
+    return get_train_data('../data/regression/train_features.csv', '../data/regression/train_label.csv', 'rating', split_label)
 
 
 def get_all_ft_data(train_ft_file: str, test_ft_file: str, train_test_split=False):
